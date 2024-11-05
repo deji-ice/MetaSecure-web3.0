6d05f8fe5bf0d9b2555dfa03c35e4e902703eddc
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-11-05 21:13:23 +0100

    commit message

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,13 +1,16 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { TransactionContext } from "../../context/TransactionsContext";
-import dummyData from "../../utils/dummyData";
 import TransactionCard from "./TransactionCard";
 
-
 const Transactions = () => {
-  const { currentAccount } = useContext(TransactionContext);
+  const { currentAccount, transactions } = useContext(TransactionContext);
+
+  useEffect(() => {
+    // This effect will run whenever the transactions array changes
+  }, [transactions]);
+
   return (
-    <div className="flex  flex-col w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
+    <div className="flex flex-col w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
       <div className="flex flex-col md:p-12 py-12 px-4">
         <h3 className="text-white text-3xl text-center my-2">
           Latest Transactions
@@ -18,13 +21,13 @@ const Transactions = () => {
           )}
         </h3>
       </div>
-      <div className="flex flex-wrap justify-center items-center mt-10 ">  
-        {dummyData.reverse().map((transaction, index) => (
+      <div className="flex flex-wrap justify-center items-center mt-10">
+        {transactions.slice().reverse().map((transaction, index) => (
           <TransactionCard transaction={transaction} key={index} />
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Transactions
+export default Transactions;
