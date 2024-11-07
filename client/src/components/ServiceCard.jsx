c090f95a999fe69e9d7b1cf297897fea2269f39a
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-11-07 09:58:50 +0100

    feat: Add ServiceCard component with basic styling

---
+++ client/src/components/ServiceCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ServiceCard = () => {
+  return (
+    <div className="flex flex-row justify-normal items-center white-glassmorphism p-3 m-2 cursor-pointer hover:shadow-xl">
+      ServiceCard
+    </div>
+  );
+};
+
+export default ServiceCard;
