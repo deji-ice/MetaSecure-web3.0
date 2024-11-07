3376567c917b0fac20fb3191344a0fe16f047b7c
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-11-07 09:59:01 +0100

    feat: Update Services component with layout and ServiceCard integration

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -1,9 +1,23 @@
-
+import { BsShieldFillCheck } from "react-icons/bs";
+import { BiSearchAlt } from "react-icons/bi";
+import { RiHeart2Fill } from "react-icons/ri";
+import ServiceCard from "./ServiceCard";
 
 const Services = () => {
   return (
-    <div>Services</div>
-  )
-}
+    <div className="flex w-full justify-center items-center gradient-bg-services ">
+      <div className=" flex md:flex-row flex-col items-center justify-between md:p-20 py-12 px-4">
+        <div className="flex-1 flex flex-col justify-start items-start">
+          <h1 className="text-white text-3xl md:text-5xl py-2 text-gradient">
+            Services that we <br /> continue to improve
+          </h1>
+        </div>
+      </div>
+      <div className="flex-1 flex flex-col justify-start items-center">
+        <ServiceCard color={"#2952e3"} title={"Services Guaranteed"}/>
+      </div>
+    </div>
+  );
+};
 
-export default Services
+export default Services;
