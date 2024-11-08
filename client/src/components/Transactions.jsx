5547c78a941a893f0da3d357adabc5e7b89a4234
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-11-08 14:23:42 +0100

    commit message

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,8 +1,22 @@
+import { useContext } from "react";
+import { TransactionContext } from "../../context/TransactionsContext";
 
 
 const Transactions = () => {
+  const { currentAccount } = useContext(TransactionContext);
   return (
-    <div>Transactions</div>
+    <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
+      <div className="flex flex-col md:p-12 py-12 px-4">
+        <h3 className="text-white text-3xl text-center my-2">
+          Latest Transactions
+          {currentAccount ? (
+            <p>Your account: {currentAccount}</p>
+          ) : (
+            <p>Connect your account to see the latest transactions</p>
+          )}
+        </h3>
+      </div>
+    </div>
   )
 }
 
