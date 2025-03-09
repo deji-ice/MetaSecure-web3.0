f44668edb24fdd209d2d37b35346157de830fb73
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2025-03-10 00:16:15 +0100

    refactor: Update import of Transactions.json to use 'abi' and log the imported value

--- client/utils/constants.js
+++ client/utils/constants.js
@@ -1,5 +1,6 @@
-import Transactions from '../utils/Transactions.json';
+import abi from "../utils/Transactions.json";
 
+console.log("ABI from import:", abi);
 // The CONTRACT_ADDRESS is set in the .env file and accessed via Vite's import.meta.env
 export const CONTRACT_ADDRESS = import.meta.env.VITE_CONTRACT_ADDRESS;
-export const CONTRACT_ABI = Transactions.abi;
+export const CONTRACT_ABI = abi.abi;
