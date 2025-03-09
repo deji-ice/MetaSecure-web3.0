b8c9c0162fd4b31db4159ca7a9fef2482b8b77c8
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2025-03-09 20:47:17 +0100

    refactor: Integrate react-hot-toast for improved user notification handling in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
-import Footer from "./components/Footer"
-import NavBar  from "./components/NavBar";
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
 import Services from "./components/Services";
 import Transactions from "./components/Transactions";
 import Welcome from "./components/Welcome";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <div className="min-h-screen">
+      <Toaster position="top-right" />
       <header className="gradient-bg-welcome">
         <NavBar />
         <Welcome />
